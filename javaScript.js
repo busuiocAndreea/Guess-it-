efa8f34f0731a038e4d8239d808ac8a41e6d911d
busuiocAndreea <101110848+busuiocAndreea@users.noreply.github.com>
2022-03-07 14:16:24 +0200

    Update javaScript.js

--- javaScript.js
+++ javaScript.js
@@ -1,15 +1,17 @@
-var randomNumber = Math.round(Math.random() * 100) + 1;
-function verifyNumber(myGuess) { 
-   if (myGuess == randomNumber) {
-      alert("You got it right!");
-   } else {
-    alert("You lost");
-   }
-} 
+var randomNumber = Math.round(Math.random() * 50) + 1;
+
+function verifyNumber(myGuess) {
+    if (myGuess == randomNumber) {
+        alert("You got it right!");
+    } else {
+        alert("You lost");
+    }
+}
+
 function buttonsAdd() {
-	var insertedNumber = document.getElementById("keyboardInsertedNo").value;
-	for(let i = 0; i < insertedNumber; i++){ 
-		document.getElementById("spaceBetweenDivs").innerHTML += '<button type = "button" id class="btn btn-outline-dark" onclick="return verifyNumber(` + i + `);">Choose me!';
-	}
-	return false;
+    var insertedNumber = document.getElementById("keyboardInsertedNo").value;
+    for (let i = 0; i < insertedNumber; i++) {
+        document.getElementById("spaceBetweenDivs").innerHTML += '<button type = "button" id class="btn btn-outline-dark" onclick="return verifyNumber(` + i + `);">Choose me!';
+    }
+    return false;
 }
